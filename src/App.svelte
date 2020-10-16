aad9f012568dc3991549204c5374012c8efd6586
Angela Gomez <angela.gomez@Angie.local>
2020-10-16 16:28:24 +0100

    2-HTML

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import HowTo from './HowTo.svelte';
+    import Timer from './Timer.svelte';  
+</script>
+
+<style>
+    h1, h3 {
+        text-align: center;
+    }
+</style>
+
+<h1>HandWashing App</h1>
+<Timer/>
+<HowTo/>
+
+<h3>
+    <a href="https://www.who.int/gpsc/clean_hands_protection/en/" target="_blank">
+      Picture Source
+    </a>
+  
+    <a href="https://freesound.org/people/metrostock99/sounds/345086/ " target="_blank">
+      Sound Source
+    </a>
+  </h3>
