b2b35db713da0b18968a32096ff6ffd7a67a694d
Angela Gomez <angela.gomez@Angie.local>
2020-10-21 22:22:19 +0100

    add Audio

--- src/App.svelte
+++ src/App.svelte
@@ -1,24 +1,37 @@
 <script>
-	import HowTo from './HowTo.svelte';
-    import Timer from './Timer.svelte';  
+  import HowTo from "./HowTo.svelte";
+  import Timer from "./Timer.svelte";
+  let audio;
+  function timerEnds(e) {
+    audio.play();
+    console.log(e);
+  }
 </script>
 
 <style>
-    h1, h3 {
-        text-align: center;
-    }
+  h1,
+  h3 {
+    text-align: center;
+  }
 </style>
 
 <h1>HandWashing App</h1>
-<Timer/>
-<HowTo/>
+<Timer on:end={timerEnds} />
+<HowTo />
 
 <h3>
-    <a href="https://www.who.int/gpsc/clean_hands_protection/en/" target="_blank">
-      Picture Source
-    </a>
-  
-    <a href="https://freesound.org/people/metrostock99/sounds/345086/ " target="_blank">
-      Sound Source
-    </a>
-  </h3>
+  <a href="https://www.who.int/gpsc/clean_hands_protection/en/" target="_blank">
+    Picture Source
+  </a>
+
+  <a
+    href="https://freesound.org/people/metrostock99/sounds/345086/ "
+    target="_blank">
+    Sound Source
+  </a>
+</h3>
+
+<!-- svelte-ignore a11y-media-has-caption -->
+<audio bind:this={audio}>
+  <source src="sound.wav" />
+</audio>
--- src/HowTo.svelte
+++ src/HowTo.svelte
@@ -1,11 +1,10 @@
 <style>
     img{
-        max-width: 100%;
+        width: 100%;
     }
 </style>
 
-<p>HowTo</p>
 <div bp="grid">
-    <img bp="offset-5@md 4@md 12@md"  
+    <img bp="offset-5@md 4@md 12@sm"  
     src="handwashing.gif" alt="How to wash your hands">
 </div>
